2c673af5d99ead8e4d623c24f1d2d2f6e8e021cd
Jacobus Badenhorst <jacobusbad@gmail.com>
2020-11-24 15:28:58 -0700

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim aliquid
+        nobis perferendis iusto! Doloribus corrupti corporis beatae ipsam
+        officia optio deleniti quam ut, fugiat recusandae quis, libero aut.
+        Blanditiis laboriosam eos porro libero excepturi ea reprehenderit! Iure
+        similique fugit laboriosam qui et, dolor aut doloremque corrupti! Dolore
+        qui suscipit quos!
+      </p>
+    </section>
+  );
+};
+
+export default About;
